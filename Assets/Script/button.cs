30d8dc605efa85c8d51b57ad1a1b5e95ca29d739
YadaK06 <tonfaiyada@gmail.com>
2026-08-25 23:27:10 +0700

    buttonandMainmenu

---
+++ Assets/Script/button.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class button : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
+    }
+}
